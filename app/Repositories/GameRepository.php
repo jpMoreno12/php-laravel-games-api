482c7f0461d0ce4be060d99a4daf3a259942bd2e
morendev <morenodasilvajoao709@gmail.com>
2025-06-05 13:27:11 -0300

    feat: create contract and create repository

---
+++ app/Repositories/GameRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Game;
+use App\Repositories\Contracts\IGameContract;
+
+class GameRepository implements IGameContract
+{
+    public Game $game;
+
+    public function __construct(Game $game)
+    {
+        $this->game = $game;
+    }
+
+    public function createGame(array $data)
+    {
+        return $this->game->create($data);
+    }
+
+    public function findGame(int $id)
+    {
+        return $this->game->find($id);
+    }
+
+    public function findAllGames()
+    {
+        $this->game->all();
+    }
+
+    public function updateGame(int $id, array $data)
+    {
+        $game = $this->game->find($id);
+        if ($game) {
+            $game->update($data);
+            return $game;
+        }
+        return false;
+    }
+
+    public function deleteGame(int $id)
+    {
+        $game = $this->game->find($id);
+        if ($game) {
+            return $game->delete();
+        }
+        return false;
+    }
+}
